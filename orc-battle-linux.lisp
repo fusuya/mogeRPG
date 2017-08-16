594f6c3f5f3219b864e5f28144bb66760cd25b78
Yoteichi <plonk@piano.email.ne.jp>
2017-08-17 00:47:13 +0900

    ダッシュ移動機能の追加。

--- orc-battle-linux.lisp
+++ orc-battle-linux.lisp
@@ -663,6 +663,18 @@
      (update-player-pos p x y map)
      (if (= (randval 10) 1) ;;敵との遭遇確率
 	 (setf *battle?* t)))))
+;; (x,y) の方向が通路だった場合は、何かに当たるか敵と遭遇するまで移動
+;; する。通路でなかった場合は、その方向に普通に移動しようとしたように
+;; 振る舞う。
+(defun update-map-dash (map p y x &optional (first-move? t))
+  (case (aref map (+ (player-posy p) y) (+ (player-posx p) x))
+    (0
+     (update-map map p y x)
+     (unless *battle?*
+       (update-map-dash map p y x nil)))
+    (otherwise
+     (when first-move?
+       (update-map map p y x)))))
 ;;薬を使う
 (defun use-heal (p)
   (cond
@@ -690,15 +702,19 @@
     (show-map map p)
     ;;(format t "~%どちらに移動しますか？[u]上 [d]下 [r]右 [l]左 [q]薬を使う: ")
     ;;(with-readline-mode
-    (case (read)
-      (w (update-map map p -1 0))
-      (s (update-map map p 1 0))
-      (d (update-map map p 0 1))
-      (a (update-map map p 0 -1))
-      (q (use-heal p))
-      (z (setf *end* 2))
-      ;;(mogezouisgod (urawaza p))
-      (otherwise
-       (format t "w,a,s,d,q,zの中から選んでください！~%")))
-
+    (let ((cmd (read-line)))
+      (cond
+        ((equal cmd "w") (update-map map p -1 0))
+        ((equal cmd "s") (update-map map p 1 0))
+        ((equal cmd "d") (update-map map p 0 1))
+        ((equal cmd "a") (update-map map p 0 -1))
+        ((equal cmd "W") (update-map-dash map p -1 0))
+        ((equal cmd "S") (update-map-dash map p 1 0))
+        ((equal cmd "D") (update-map-dash map p 0 1))
+        ((equal cmd "A") (update-map-dash map p 0 -1))
+        ((equal cmd "q") (use-heal p))
+        ((equal cmd "z") (setf *end* 2))
+        ;((equal cmd "mogezouisgod") (urawaza p))
+        (t
+         (format t "w,a,s,d,q,zの中から選んでください！~%"))))
     (map-move map p)))
